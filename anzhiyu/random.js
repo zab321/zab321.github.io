08482e225ba17231dac5c5f5b77edd37b7aeced5
zab321 <19337147300@163.com>
2026-01-13 10:15:28 +0800

    Site updated: 2026-01-13 10:15:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3eeb.html","posts/13178.html","posts/10219.html","posts/7737.html","posts/64332.html"];function toRandomPost(){
+var posts=["posts/3eeb.html","posts/64332.html","posts/13178.html","posts/10219.html","posts/7737.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
